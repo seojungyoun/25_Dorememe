7f446b5b2f72d45406c37f325e36a38247d07e20
younnii <132765712+seojungyoun@users.noreply.github.com>
2025-10-05 21:13:43 +0900

    csv export 수정

--- Dorememe3/Assets/VRPen/VRPen.cs
+++ Dorememe3/Assets/VRPen/VRPen.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:631cb47dc78e6cd4bcf80adbd38308f2266eec1237cb3a58f48c5c42917f878f
-size 14220
+oid sha256:72c7745e5985837f7c6c3259c4595f0a6696cbf101346ab2a3c80f266b017418
+size 13801
